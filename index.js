7f5fac7929e88fec732a5b131533488cdb158abf
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-02 05:21:14 +0530

    Revising the basics of problem solving with students

---
+++ index.js
@@ -0,0 +1,6 @@
+//When the user inputs a number
+//Loop from 1 to the user input number
+//If the current number is divisible by 3 then print "Fizz"
+//If the current number is divisible by 5 then print "Buzz"
+//If the current number is divisible by 3 and 5 then print "FizzBuzz"
+//Otherwise just print the current number
