f1d8c6628d2803d1aa7e414609f602db54c340e4
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-02 05:41:24 +0530

    Prints "FizzBuzz" if both conditions are true

--- index.js
+++ index.js
@@ -8,7 +8,10 @@
 let answer = parseInt(prompt("Please enter a number of your choice: "));
 
 for (let i=1; i <= answer; i++) {
-    if(i % 3 === 0) {
+    if(i % 3 == 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 === 0) {
         console.log("Fizz");
     }
     else if(i % 5 === 0) {
